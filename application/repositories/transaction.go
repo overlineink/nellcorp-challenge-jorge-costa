d262c3680290ed52251b49b2c15c3ef5ac4ba3b6
Jorge <jorgedacostaza@gmail.com>
2023-11-10 08:39:33 +0100

    feat(application/repositories): add transaction repository

---
+++ application/repositories/transaction.go
@@ -0,0 +1,8 @@
+package repositories
+
+import "github.com/overlineink/nellcorp-challenge-jorge-costa/domain/entities"
+
+type TransactionRepository interface {
+	Register(transaction *entities.Transaction) error
+	Save(transaction *entities.Transaction) error
+}
