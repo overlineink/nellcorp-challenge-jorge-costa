db9b041ea4d9600e79da75fc35688f0e26adb0fc
Jorge <jorgedacostaza@gmail.com>
2023-11-10 08:39:40 +0100

    feat(application/repositories): add account repository

---
+++ application/repositories/account.go
@@ -0,0 +1,7 @@
+package repositories
+
+import "github.com/overlineink/nellcorp-challenge-jorge-costa/domain/entities"
+
+type AccountRepository interface {
+	FindAccountById(id string) (*entities.Account, error)
+}
