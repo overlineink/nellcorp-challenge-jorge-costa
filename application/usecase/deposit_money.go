c47342cbf09b541f64e52a7f5d3278a4e8a8240b
Jorge <jorgedacostaza@gmail.com>
2023-11-10 08:53:26 +0100

    feat(application/usecase): deposit money

---
+++ application/usecase/deposit_money.go
@@ -0,0 +1,36 @@
+package usecase
+
+import (
+	"errors"
+	"github.com/overlineink/nellcorp-challenge-jorge-costa/application/repositories"
+	"github.com/overlineink/nellcorp-challenge-jorge-costa/domain/entities"
+	"sync"
+)
+
+type DepositMoney struct {
+	accountRepository     repositories.AccountRepository
+	transactionRepository repositories.TransactionRepository
+}
+
+func (u *DepositMoney) Execute(accountId string, amount float64, description string, processTransactionChan chan<- *entities.Transaction, wg *sync.WaitGroup) error {
+	defer wg.Done()
+
+	account, errFindingAccount := u.accountRepository.FindAccountById(accountId)
+	if errFindingAccount != nil {
+		return errFindingAccount
+	}
+
+	transaction, errBuildingTransaction := entities.NewTransaction(entities.MoneyDeposit, amount, account, nil, description)
+	if errBuildingTransaction != nil {
+		return errBuildingTransaction
+	}
+
+	err := u.transactionRepository.Register(transaction)
+	if err != nil {
+		return errors.New("unable to register this transaction")
+	}
+
+	processTransactionChan <- transaction
+
+	return nil
+}
