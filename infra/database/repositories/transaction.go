2af3845ea106e657be61f2531cfbe73cafb20bc1
Jorge <jorgedacostaza@gmail.com>
2023-11-11 20:21:36 +0100

    feat: implements transaction repository contract for Postgres

---
+++ infra/database/repositories/transaction.go
@@ -0,0 +1,37 @@
+package repositories
+
+import (
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/overlineink/nellcorp-challenge-jorge-costa/domain/entities"
+)
+
+type TransactionRepositoryDb struct {
+	Db *gorm.DB
+}
+
+func (r *TransactionRepositoryDb) Register(transaction *entities.Transaction) error {
+	err := r.Db.Create(transaction).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TransactionRepositoryDb) Save(transaction *entities.Transaction) error {
+	err := r.Db.Save(transaction).Error
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *TransactionRepositoryDb) FindById(id string) (*entities.Transaction, error) {
+	var transaction entities.Transaction
+	r.Db.Preload("Account").First(&transaction, "id = ?", id)
+
+	if transaction.ID == "" {
+		return nil, fmt.Errorf("no transaction was found")
+	}
+	return &transaction, nil
+}
