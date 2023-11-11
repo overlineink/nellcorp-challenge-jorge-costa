185086750b991646234552fc111a4c54cf6e7ed3
Jorge <jorgedacostaza@gmail.com>
2023-11-11 20:22:12 +0100

    feat: bind gorm constraints for db

--- core/domain/entity.go
+++ core/domain/entity.go
@@ -3,7 +3,7 @@ package domain
 import "time"
 
 type Entity struct {
-	ID        string    `json:"id" valid:"notnull"`
-	CreatedAt time.Time `json:"created_at" valid:"-"`
-	UpdatedAt time.Time `json:"updated_at" valid:"-"`
+	ID        string    `json:"id" valid:"notnull" gorm:"type:uuid;primary_key"`
+	CreatedAt time.Time `json:"created_at" valid:"-" gorm:"column:created_at;type:time"`
+	UpdatedAt time.Time `json:"updated_at" valid:"-" gorm:"column:updated_at;type:time"`
 }
